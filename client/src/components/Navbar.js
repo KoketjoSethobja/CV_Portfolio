df993a904cb31c050684491e532002644aa3540d
KoketjoSethobja <sethobjakoketjo90@gmail.com>
2021-11-23 12:15:09 +0200

    added tailwindcss and other packages

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="bg-gray-800">
+                
+            </nav>
+        </div>
+    )
+}
+ 
+export default Navbar;
